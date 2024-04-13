9281a51ba656a3b90dcd849649f5ff357ac366a1
David Cardenas <davidcarguz@gmail.com>
2024-04-13 10:38:33 -0500

    Added dynamic size support based on viewport size and implemented it in login and courses header

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return const Scaffold(
-      body: SafeArea(child: LoginPage()),
+      body: LoginPage(),
     );
   }
 }
--- lib/screens/courses_screen.dart
+++ lib/screens/courses_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/size_config.dart';
+
 class CoursesScreen extends StatelessWidget {
   static const route = 'courses';
 
@@ -7,33 +9,37 @@ class CoursesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 50.0),
+      body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Icon(Icons.menu),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: Container(
-                    alignment: Alignment.center,
-                      child: Text(
-                    'Courses',
-                    style: TextStyle(fontSize: 20.0),
-                  )),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: CircleAvatar(
-                    child: Icon(Icons.person),
+            Padding(
+              padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 2.0,
+              bottom: SizeConfig.blockSizeVertical * 1.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Icon(Icons.menu),
                   ),
-                ),
-              ],
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      alignment: Alignment.center,
+                        child: Text(
+                      'Courses',
+                      style: TextStyle(fontSize: 20.0),
+                    )),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: CircleAvatar(
+                      child: Icon(Icons.person),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Divider(),
           ],
