94eb83b5825ade6fc3b6d151cd23aadbbfcda11a
David Cardenas <davidcarguz@gmail.com>
2024-04-14 15:52:54 -0500

    refactored user header to a component file

---
+++ lib/components/user_header.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../utils/size_config.dart';
+
+class UserHeader extends StatelessWidget {
+  const UserHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 2.0,
+              bottom: SizeConfig.blockSizeVertical * 1.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Icon(Icons.menu),
+              ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Courses',
+                      style: TextStyle(fontSize: 20.0),
+                    )),
+              ),
+              Expanded(
+                flex: 1,
+                child: CircleAvatar(
+                  child: Icon(Icons.person),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Divider(),
+      ],
+    );
+  }
+}
--- lib/screens/courses_screen.dart
+++ lib/screens/courses_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../utils/size_config.dart';
+import '../components/user_header.dart';
 
 class CoursesScreen extends StatelessWidget {
   static const route = 'courses';
@@ -9,41 +8,9 @@ class CoursesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical * 2.0,
-              bottom: SizeConfig.blockSizeVertical * 1.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Icon(Icons.menu),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      alignment: Alignment.center,
-                        child: Text(
-                      'Courses',
-                      style: TextStyle(fontSize: 20.0),
-                    )),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: CircleAvatar(
-                      child: Icon(Icons.person),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Divider(),
-          ],
-        ),
+        child: UserHeader(),
       ),
     );
   }
